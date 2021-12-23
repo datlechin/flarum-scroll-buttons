59e1d1c0be180e1b413ee490c52e78501879f317
Ngo Quoc Dat <datlechin@gmail.com>
2021-12-23 19:12:20 +0700

    Increase spacing between `ScrollButtons` with `acpl/mobile-tab` and `itnt-uitab` extensions

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -7,6 +7,7 @@ app.initializers.add('datlechin/flarum-scroll-buttons', () => {
   extend(ForumApplication.prototype, 'mount', () => {
     const div = document.createElement('div');
     div.classList.add('ScrollButtons');
+    app.initializers.has('acpl/mobile-tab') || app.initializers.has('itnt-uitab') ? div.classList.add('has-MobileTab') : null;
     m.mount(document.body.appendChild(div), ScrollButton);
   });
 });
